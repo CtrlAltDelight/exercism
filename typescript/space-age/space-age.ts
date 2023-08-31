53539ae5f1b7d5be8268ec44ec4541a97c205ec0
CtrlAltDelight <lbchigges@gmail.com>
2023-08-31 12:07:43 -0400

    Finished space-age

---
+++ typescript/space-age/space-age.ts
@@ -0,0 +1,14 @@
+export function age(planet: string, seconds: number): number {
+	let planetYears = {
+		"mercury": 0.2408467,
+		"venus": 0.61519726,
+		"earth": 1.0,
+		"mars": 1.8808158,
+		"jupiter": 11.862615,
+		"saturn": 29.447498,
+		"uranus": 84.016846,
+		"neptune": 164.79132,
+	};
+
+	return Number((seconds / 31557600 / planetYears[planet as keyof typeof planetYears]).toFixed(2));
+}
